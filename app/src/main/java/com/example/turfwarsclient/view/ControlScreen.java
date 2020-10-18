bf36980c66bddcebc44e3d268c9e6d7c60785e1d
Jhon S Garcia Izquierdo <metalprime57@gmail.com>
2020-10-18 12:11:31 -0500

    update ControlScreen

--- app/src/main/java/com/example/turfwarsclient/view/ControlScreen.java
+++ app/src/main/java/com/example/turfwarsclient/view/ControlScreen.java
@@ -3,11 +3,15 @@ package com.example.turfwarsclient.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.turfwarsclient.R;
+import com.example.turfwarsclient.model.Coordinate;
+import com.example.turfwarsclient.model.OnMessageListener;
+import com.google.gson.Gson;
 
-public class ControlScreen extends AppCompatActivity {
+public class ControlScreen extends AppCompatActivity implements View.OnClickListener, OnMessageListener {
 
     private Button btnUp;
     private Button btnDown;
@@ -20,9 +24,24 @@ public class ControlScreen extends AppCompatActivity {
     private Button btnWeaponShotgun;
     private Button btnWeaponPistol;
 
+    private Coordinate coordinate;
+    private Gson gson;
+    private String line;
+    private boolean buttonPressed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_screen);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void OnMessage(String msg) {
+
     }
 }
